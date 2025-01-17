37e7f1aff65a6686dc785178edc40e28b4fa4c20
rektgit2024 <myemailispatrick@gmail.com>
2025-01-17 21:03:18 +0800

    refactor(order): make Order component reusable for fetching orders by status

--- src/pages/user/MyPurchasePage/components/CancelledOrders.tsx
+++ src/pages/user/MyPurchasePage/components/CancelledOrders.tsx
@@ -1,8 +1,5 @@
-import { Box, Skeleton, Spinner } from "@chakra-ui/react";
-import InfiniteScroll from "react-infinite-scroll-component";
 import useGetAllOrderItems from "../hooks/useGetAllOrderItems";
-import NoOrdersYet from "./NoOrdersYet";
-import OrderCard from "./OrderCard";
+import Orders from "./Orders";
 
 const CancelledOrders = () => {
   const { data, fetchNextPage, hasNextPage, isLoading } = useGetAllOrderItems({
@@ -10,43 +7,13 @@ const CancelledOrders = () => {
     status: "CANCELLED",
   });
 
-  const ordersLength =
-    data?.pages.flatMap((order) => order.orderModels).length || 0;
-
-  const fetchOrderData =
-    data?.pages.reduce((total, page) => total + page.orderModels.length, 0) ||
-    0;
-  const array = [1, 2, 3, 4, 5];
-
-  if (isLoading) {
-    return (
-      <>
-        {array.map((skel) => (
-          <Skeleton key={skel} height="200px" mt="10px" />
-        ))}
-      </>
-    );
-  }
-
-  if (ordersLength < 1) {
-    return <NoOrdersYet />;
-  }
-
   return (
-    <Box>
-      <InfiniteScroll
-        dataLength={fetchOrderData}
-        next={fetchNextPage}
-        hasMore={!!hasNextPage}
-        loader={<Spinner />}
-      >
-        {data?.pages.map((page) =>
-          page.orderModels.map((order) => (
-            <OrderCard key={order.orderId} order={order} />
-          ))
-        )}
-      </InfiniteScroll>
-    </Box>
+    <Orders
+      data={data}
+      isLoading={isLoading}
+      fetchNextPage={fetchNextPage}
+      hasNextPage={hasNextPage}
+    />
   );
 };
 
